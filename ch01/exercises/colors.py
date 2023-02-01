d486c5f77726348335734633236ef612840b9a1d
Emma Moy <emoy5@binghamton.edu>
2023-02-01 17:24:44 -0500

    sdfghj

--- ch01/exercises/colors.py
+++ ch01/exercises/colors.py
@@ -6,5 +6,9 @@ colors = [
     "yellow"
 ]
 
-print(len(colors))
+print("I have ", len(colors), " colors to choose from.")
 print(colors[0], colors[1], colors[2],colors[3],colors[4], sep="\n")
+
+"""or you could do 
+print(colors[0])
+print(colors[1]) """
