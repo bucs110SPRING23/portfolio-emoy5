1b77dce089a614425c662e7b8ccb0528eb6f3868
Emma Moy <emoy5@binghamton.edu>
2023-02-13 17:34:17 -0500

    sdfghj

--- ch02/exercises/shapes.py
+++ ch02/exercises/shapes.py
@@ -8,14 +8,14 @@ pygame.display.flip()
 pygame.draw.circle(screen, "white", [500,750], 250)
 pygame.display.flip()
 
-pygame.draw.circle(screen, "black", [500,500], 152)
+pygame.draw.circle(screen, "black", [500,450], 152)
 pygame.display.flip()
-pygame.draw.circle(screen, "white", [500,500], 150)
+pygame.draw.circle(screen, "white", [500,450], 150)
 pygame.display.flip()
 
-pygame.draw.circle(screen, "black", [500,350], 102)
+pygame.draw.circle(screen, "black", [500,250], 102)
 pygame.display.flip()
-pygame.draw.circle(screen, "white", [500,350], 100)
+pygame.draw.circle(screen, "white", [500,250], 100)
 pygame.display.flip()
 
 pygame.time.wait(2000)
