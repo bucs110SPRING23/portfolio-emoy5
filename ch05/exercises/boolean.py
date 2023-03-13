d20d0bdcf0e59b0d16c4f49873ae07b5d99d7b9e
Emma Moy <emoy5@binghamton.edu>
2023-03-13 17:37:13 -0400

    dfgh

--- ch05/exercises/boolean.py
+++ ch05/exercises/boolean.py
@@ -1,4 +1,9 @@
 def percentage_to_letter(percent=99):
+    """ Called a docstring - different from a comment
+    This is a function that returns a letter grade based on a percentage
+    args: percent (int)
+    return: letter (str)
+    """
     let = "A"
     if 80 <= percent < 90:
         let = "B"
@@ -11,9 +16,15 @@ def percentage_to_letter(percent=99):
     return let
     
 def is_passing(letter): #boolean functions bc they return booleans, is_ *convention
+    """ 
+    This is a function that returns a letter grade based on a percentage
+    args: percent (int)
+    return: letter (str)
+    """
     return letter.lower() in "abc" # "in" is a boolean operator by def so this whole function will return either a true or a false
 
 def main():
+    #no docstring needed 
     grades = [90, 80, 70, 60, 50]
     for grade in grades:
         letter = percentage_to_letter(grade)
@@ -23,3 +34,4 @@ def main():
             print("Someone messed up you grades")
 
 main()
+print(is_passing.__doc__)
